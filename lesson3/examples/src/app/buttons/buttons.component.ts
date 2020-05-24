b1ad4764ed2e163d566cb44bfad6173e0b255202
Kalinka Georgieva <timbarrrli@iMac.local>
2020-05-24 13:37:42 -0400

    Completed Examples Lesson 3

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'buttons',
-   templateUrl: './buttons.component.html',
-   styleUrls: ['./buttons.component.css']
+  selector: "buttons",
+  templateUrl: "./buttons.component.html",
+  styleUrls: ["./buttons.component.css"],
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+  buttonHeading: string = "Buttons";
+  inactive: boolean = false;
+  inactiveSilver: boolean = false;
+  inactiveCooper: boolean = false;
+  constructor() {}
 
-   constructor() { }
-
-   ngOnInit() { }
-
+  ngOnInit() {}
+  reactivate(x: boolean): void {
+    this.inactive = x;
+    this.inactiveSilver = x;
+    this.inactiveCooper = x;
+  }
 }
